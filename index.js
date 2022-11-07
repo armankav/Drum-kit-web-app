d7ccc5a50c5c8227fc99320ad8e43fc2331a55d2
armankav <85289794+armankav@users.noreply.github.com>
2022-11-07 12:48:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+//other way of using function
+// document.querySelectorAll("button").addEventListener("click",clickbtn);
+//
+// function clickbtn()
+// {
+//   alert("i was clicked");
+// }
+
+var limit=document.querySelectorAll(".drum").length;
+
+for(var i=0;i<limit;i++)
+{
+document.querySelectorAll("button")[i].addEventListener("click",function()
+{
+var arman=this.innerHTML;
+makeSound(arman);
+buttonAnimation(arman);
+
+});
+
+}
+
+document.addEventListener("keydown",function(event)
+{
+makeSound(event.key);
+buttonAnimation(event.key);
+});
+
+
+function makeSound(key)
+{
+  switch (key) {
+    case "w":
+    var audio=new Audio("sounds/crash.mp3");
+    audio.play();
+    break;
+
+    case "a":
+    var audio=new Audio("sounds/kick-bass.mp3");
+    audio.play();
+    break;
+
+    case "s":
+    var audio=new Audio("sounds/snare.mp3");
+    audio.play();
+    break;
+
+    case "d":
+    var audio=new Audio("sounds/tom-1.mp3");
+    audio.play();
+    break;
+
+    case "j":
+    var audio=new Audio("sounds/tom-2.mp3");
+    audio.play();
+    break;
+
+    case "k":
+    var audio=new Audio("sounds/tom-3.mp3");
+    audio.play();
+    break;
+
+    case "l":
+    var audio=new Audio("sounds/tom-4.mp3");
+    audio.play();
+    break;
+
+    default:
+
+  }
+}
+
+function buttonAnimation(currentKey)
+{
+  var activeButton=document.querySelector("."+ currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function()
+{
+  activeButton.classList.remove("pressed");
+},500);
+}
